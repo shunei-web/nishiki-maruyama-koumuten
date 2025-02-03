fdfe35ad32572080aa5447865ad91b2df9c8c676
shunei-web <6555587+shunei-web@users.noreply.github.com>
2025-02-04 01:43:33 +0900

    sassにmixinとvariableを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,18 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `\
+            @use "/src/styles/sass/global/_index.scss" as global;\
+            @use "/src/styles/sass/style.scss" as *;\
+            `,
+        },
+      },
+    },
+  },
   integrations: [
     {
       name: "importStyleCSS",
