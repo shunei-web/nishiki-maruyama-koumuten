63e015576e7d21fa67fd2e75ad6b7217192df1bc
shunei-web <6555587+shunei-web@users.noreply.github.com>
2025-02-03 22:29:51 +0900

    リセットCSSの読み込み方法を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    {
+      name: "importStyleCSS",
+      hooks: {
+        "astro:config:setup": ({ injectScript }) => {
+          injectScript("page-ssr", `import '@acab/reset.css';`);
+        },
+      },
+    },
+  ],
+});
