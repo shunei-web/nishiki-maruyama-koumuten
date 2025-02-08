c06a3ea7f43c88b395ee9b9a5b21d691242a36dd
shunei-web <6555587+shunei-web@users.noreply.github.com>
2025-02-08 12:31:29 +0900

    ビルド出力をサブディレクトリに

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+const subfolder = "/murayama-koumuten/";
+
 // https://astro.build/config
 export default defineConfig({
+  base: subfolder,
+  outDir: `./dist${subfolder}`,
   vite: {
     css: {
       preprocessorOptions: {
